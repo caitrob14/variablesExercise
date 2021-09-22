4f4fe5eb86ecbc94d2f857ad3ac355ab7e6b5f63
Caitlin Robertson <robertson0630@gmail.com>
2021-09-22 08:36:29 -0500

    moved script.js to the correct folder

---
+++ VariablesExercise/script.js
@@ -0,0 +1,18 @@
+// #1
+// const
+// #2
+let num = 20;
+// #3
+num = 15;
+// #4
+num = num + 7;
+// #5
+num--;
+// #6
+num = "This is not a number";
+// #7
+let blank;
+// #8
+console.log(blank);
+// #9
+blank = null;
