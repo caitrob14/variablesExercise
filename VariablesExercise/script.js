ce9b1da0a563f28f1756375fa42adb490fc6dc25
Caitlin Robertson <robertson0630@gmail.com>
2021-09-22 09:05:30 -0500

    completed any errors

--- VariablesExercise/script.js
+++ VariablesExercise/script.js
@@ -6,13 +6,17 @@ let num = 20;
 num = 15;
 // #4
 num = num + 7;
+//num += 7;
+
 // #5
 num--;
 // #6
 num = "This is not a number";
+console.log(num);
 // #7
 let blank;
 // #8
 console.log(blank);
 // #9
 blank = null;
+
